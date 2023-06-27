c7a9824f439b7826e46b2b3046802a9dad248014
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-06-27 17:55:34 +0400

    added delate modal and function

---
+++ src/scenes/DelateModal/DelateModal.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BtnCont, Cancel, ConfirmMessage, ConfirmWrapper, DElateContainer, DelTitle, Delate } from './styles/modal'
+interface Props {
+    commentId:number
+    replyId:number
+    setIsOpenModal: React.Dispatch<React.SetStateAction<boolean>>
+    handleConfirmDelete:() => void
+}
+function DelateModal({commentId,replyId,setIsOpenModal,handleConfirmDelete}:Props) {
+    
+  return (
+    <ConfirmWrapper>
+        <DElateContainer>
+            <DelTitle>Delete comment</DelTitle>
+            <ConfirmMessage>Are you sure you want to delete this comment? This will remove the comment and can't be undone.</ConfirmMessage>
+            <BtnCont>
+                <Cancel onClick={() => setIsOpenModal(false)}>Cancel</Cancel>
+                <Delate  onClick={handleConfirmDelete}>Delate</Delate>
+            </BtnCont>
+        </DElateContainer>
+    </ConfirmWrapper>
+  )
+}
+
+export default DelateModal
