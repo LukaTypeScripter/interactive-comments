df58a7bdfa7137982fb9c74b6f6e3b165352221b
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-06-28 16:13:08 +0400

    took some styles

---
+++ src/components/styles/commentVotes.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+
+export const PlusAndMinusBtn = styled.div`
+background-color: transparent;
+border: 0;
+cursor: pointer;
+display: flex;
+height: 30px;
+place-items: center;
+width: 30px;
+justify-content: center;
+`
+export const VotesCouter = styled.div`
+color: #5457b6;
+font-size: 1.1rem;
+font-weight: 500;
+margin-block: 8px;
+`
